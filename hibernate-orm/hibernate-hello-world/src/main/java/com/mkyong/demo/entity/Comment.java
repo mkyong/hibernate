f96baaeccebbe10b86fcb6a375f6cbfb2421b332
mkyong <mkyong2002@gmail.com>
2020-02-03 21:06:23 +0800

    init

---
+++ hibernate-orm/hibernate-hello-world/src/main/java/com/mkyong/demo/entity/Comment.java
@@ -0,0 +1,64 @@
+package com.mkyong.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String text;
+
+    @ManyToOne
+    private User createdBy;
+
+    private LocalDateTime createdDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", createdBy=" + createdBy +
+                ", createdDate=" + createdDate +
+                '}';
+    }
+}
---
+++ hibernate-orm/hibernate-hello-world/src/main/java/com/mkyong/demo/entity/User.java
@@ -0,0 +1,67 @@
+package com.mkyong.demo.entity;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String email;
+
+    private String name;
+
+    @OneToMany(mappedBy = "createdBy")
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    @OrderColumn(name = "comments_index") // Index necessary to know which element to lazy-init!
+    private Set<Comment> comments = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
